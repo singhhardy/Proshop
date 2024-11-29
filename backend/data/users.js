447d7e63dff6d794bf0a87a544d043784ac84c5d
hardikWottaweb <hardik.wottaweb@gmail.com>
2024-11-29 10:09:04 +0530

    models & dummy data

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bryptjs from 'bcryptjs';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John doe',
+        email: 'jhon@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    },
+    {
+        name: 'Jane doe',
+        email: 'jane@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false,
+    }
+]
+
+export default users 
